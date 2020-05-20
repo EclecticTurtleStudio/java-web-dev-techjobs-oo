7fe12e0c0a5d8eeab74d40a7be3718f692e96351
Becky Jo Roth <eclecticturtlestudio@gmail.com>
2020-05-20 17:39:16 -0500

    Step 4 Complete - Verify Parts of Job Class

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,36 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void emptyTest(){
+        assertEquals(10,10,.001);
+    }
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertEquals(1, (job2.getId() - job1.getId()));
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job3 instanceof Job);
+        assertEquals(job3.getName(), "Product tester");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job4 = new Job("Sandwich Artist", new Employer("Subway"), new Location("Mars"), new PositionType("Customer Service"), new CoreCompetency("Personable"));
+        Job job5 = new Job("Sandwich Artist", new Employer("Subway"), new Location("Mars"), new PositionType("Customer Service"), new CoreCompetency("Personable"));
+        assertNotSame(job4, job5);
+    }
+}
